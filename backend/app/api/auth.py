0db87e7ca5726de02fd7e8b10e062e778c8c09fd
Łukasz Puzio <lukpuzio@gmail.com>
2026-06-10 14:56:28 +0200

    Implement JWT login and current user endpoint

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -8,6 +8,11 @@ from app.models.user import User
 from app.database.session import get_db
 from app.core.security import hash_password
 
+from app.schemas.user import UserCreate, UserLogin
+
+from app.core.security import hash_password, verify_password
+from app.core.auth import create_access_token, get_current_user
+
 router = APIRouter()
 
 
@@ -39,3 +44,47 @@ def register(
     return {
         "message": "User created"
     }
+    
+    
+@router.post("/login")
+def login(
+    user: UserLogin,
+    db: Session = Depends(get_db)
+):
+    existing_user = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if not existing_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    if not verify_password(user.password, existing_user.password_hash):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    access_token = create_access_token(
+        data={
+            "sub": str(existing_user.id)
+        }
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
+    
+@router.get("/me")
+def get_me(
+    current_user: User = Depends(get_current_user)
+):
+    return {
+        "id": current_user.id,
+        "email": current_user.email,
+        "is_verified": current_user.is_verified
+    }
+    
---
+++ backend/app/core/auth.py
@@ -0,0 +1,45 @@
+from datetime import datetime, timedelta
+
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from sqlalchemy.orm import Session
+
+from app.database.session import get_db
+from app.models.user import User
+
+
+
+SECRET_KEY = "SUPER_SECRET_KEY_CHANGE_ME"
+ALGORITHM = "HS256"
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(hours=24)
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db),
+):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id = payload.get("sub")
+
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    user = db.query(User).filter(User.id == int(user_id)).first()
+
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not found")
+
+    return user
