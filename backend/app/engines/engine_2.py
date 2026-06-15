4d6015cbacbf118a80e94a110787ee04d8c0bd37
Łukasz Puzio <lukpuzio@gmail.com>
2026-06-15 22:00:29 +0200

    Add multiple engine support

---
+++ backend/app/engines/engine_2.py
@@ -0,0 +1,5 @@
+def run_engine(input_data: str) -> str:
+    number = int(input_data)
+    result = number * number
+
+    return f"Engine 2 result: {result}"
--- backend/app/tasks/jobs.py
+++ backend/app/tasks/jobs.py
@@ -1,15 +1,17 @@
-import time
-from unittest import result
-
-
 from app.tasks.celery_app import celery_app
+
 from app.database.connection import SessionLocal
+
 from app.models.user import User
 from app.models.job import Job
-from app.engines.engine_1 import run_engine
+
+from app.engines.engine_1 import run_engine as run_engine_1
+from app.engines.engine_2 import run_engine as run_engine_2
+
 
 @celery_app.task(name="app.tasks.jobs.run_job")
 def run_job(job_id: int):
+
     db = SessionLocal()
 
     try:
@@ -21,12 +23,31 @@ def run_job(job_id: int):
         job.status = "RUNNING"
         db.commit()
 
-        result = run_engine(job_id)
+        if job.engine_type == "engine_1":
+            result = run_engine_1(job.input_data)
+
+        elif job.engine_type == "engine_2":
+            result = run_engine_2(job.input_data)
+
+        else:
+            job.status = "FAILED"
+            job.result = "Unknown engine type"
+            db.commit()
+            return
 
         job.result = result
         job.status = "COMPLETED"
 
         db.commit()
+
+    except Exception as e:
+
+        job = db.query(Job).filter(Job.id == job_id).first()
+
+        if job:
+            job.status = "FAILED"
+            job.result = str(e)
+            db.commit()
 
     finally:
         db.close()
