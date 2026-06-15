f8a88126279fc897c5b5c984a1a75de34e15652a
Łukasz Puzio <lukpuzio@gmail.com>
2026-06-15 22:11:32 +0200

    Validate engine type

--- backend/app/schemas/job.py
+++ backend/app/schemas/job.py
@@ -1,8 +1,15 @@
+from enum import Enum
 from pydantic import BaseModel
 
 
+class EngineType(str, Enum):
+    engine_1 = "engine_1"
+    engine_2 = "engine_2"
+    engine_3 = "engine_3"
+
+
 class JobCreate(BaseModel):
-    engine_type: str
+    engine_type: EngineType
     input_data: str
 
 
